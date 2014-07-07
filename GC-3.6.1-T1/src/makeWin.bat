b0ea4ebf981c430c74a6b28d075ce27d404e3c96
LETARTARE <LETARTARE@users.noreply.github.com>
2014-07-07 19:47:20 +0200

    - update 'README'

--- GC-3.6.1-T1/src/makeWin.bat
+++ GC-3.6.1-T1/src/makeWin.bat
@@ -3,9 +3,6 @@ rem LETARTARE
 
 rem  3.6.1-Tx
 
-set QMAKESPEC= %PATH_QT%\mkspecs\win32-g++
-set PATH=%PATH%; %PATH_QT%\bin;%PATH_MINW32%\bin
-
 rem for release
 
 qmake -config release -spec win32-g++ GcodeSenderGUIthreads.pro
